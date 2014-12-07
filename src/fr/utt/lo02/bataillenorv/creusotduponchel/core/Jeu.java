d9c1247e67e95a11c4c54ea74e1e3bb353ec1255
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2014-12-07 20:16:19 +0100

    ajout element classe Jeu

--- src/fr/utt/lo02/bataillenorv/creusotduponchel/core/Jeu.java
+++ src/fr/utt/lo02/bataillenorv/creusotduponchel/core/Jeu.java
@@ -1,14 +1,37 @@
 package fr.utt.lo02.bataillenorv.creusotduponchel.core;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Jeu {
 	private Iterator<Joueur> tourJeu;
 	private List<Joueur> listeDeJoueurs;
+	private Queue<Carte> pioche;
+	private Collection<Carte> tas;
+	
+	
 	
 	public Jeu(LinkedList<Joueur> listeDeJoueurs){
-	//	this.tourJeu=new Iterator<Joueur>();
 		this.listeDeJoueurs=listeDeJoueurs;
+		tas = new LinkedList<>();
+		pioche = new LinkedList<>();
 	}
+	
+	/**
+	 * Initialise les cartes du jeu et les ajoutes dans la pioche
+	 */
+	private void initCartes() {
+		
+	}
+	
+	/**
+	 * Lance la partie
+	 */
+	public void start() {
+		
+	}
+	
+	
 }
