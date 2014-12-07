d8983d7bf7ed84a3c4bc6412a8b0322ceceea7df
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2014-12-07 17:46:40 +0100

    CarteSpeciale -> Abstract

--- src/fr/utt/lo02/bataillenorv/creusotduponchel/core/CarteSpeciale.java
+++ src/fr/utt/lo02/bataillenorv/creusotduponchel/core/CarteSpeciale.java
@@ -1,11 +1,10 @@
 package fr.utt.lo02.bataillenorv.creusotduponchel.core;
-public class CarteSpeciale extends Carte {
+public abstract class CarteSpeciale extends Carte {
+	
 	public CarteSpeciale(int valeur) {
 		super(valeur);
 	}
 	
-	public void effet(Jeu jeu){
-		
-	}
+	public abstract void effet(Jeu jeu);
 
 }
