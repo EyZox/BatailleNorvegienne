9f66f6c9b30146601eadedba50c1a895a37e46d4
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2014-12-18 22:43:52 +0100

    TourJeu est maintenant cloneable

--- src/fr/utt/lo02/bataillenorv/creusotduponchel/core/TourJeu.java
+++ src/fr/utt/lo02/bataillenorv/creusotduponchel/core/TourJeu.java
@@ -3,7 +3,7 @@ package fr.utt.lo02.bataillenorv.creusotduponchel.core;
 import java.util.Iterator;
 import java.util.List;
 
-public class TourJeu implements Iterator<Joueur> {
+public class TourJeu implements Iterator<Joueur>, Cloneable{
 
 	private List<Joueur> joueurs;
 	private int index = -1;
@@ -19,7 +19,7 @@ public class TourJeu implements Iterator<Joueur> {
 
 	@Override
 	public Joueur next() {
-		if(index >= joueurs.size()) index = -1;
+		if(index >= joueurs.size()-1) index = -1;
 		return joueurs.get(++index);
 	}
 
@@ -27,4 +27,16 @@ public class TourJeu implements Iterator<Joueur> {
 	public void remove() {
 	}
 
+	@Override
+	public Object clone() {
+		TourJeu t = null;
+		try {
+			t = (TourJeu) super.clone();
+		}catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+			return null;
+		}
+		t.joueurs = joueurs;
+		return t;
+	}
 }
