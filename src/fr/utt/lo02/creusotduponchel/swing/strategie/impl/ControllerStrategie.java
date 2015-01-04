a1be8ed5b57f2933885cadef1a5666fc9f746e07
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2015-01-04 17:34:27 +0100

    Ajout de la strategie graphique

---
+++ src/fr/utt/lo02/creusotduponchel/swing/strategie/impl/ControllerStrategie.java
@@ -0,0 +1,5 @@
+package fr.utt.lo02.creusotduponchel.swing.strategie.impl;
+
+public class ControllerStrategie {
+
+}
---
+++ src/fr/utt/lo02/creusotduponchel/swing/strategie/impl/VueStrategie.java
@@ -0,0 +1,7 @@
+package fr.utt.lo02.creusotduponchel.swing.strategie.impl;
+
+import javax.swing.JPanel;
+
+public class VueStrategie extends JPanel {
+
+}
